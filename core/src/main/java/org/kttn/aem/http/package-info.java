ddff67428c38900e2c8e46e73cfc259944895d64
Florian Brunswicker <kttn@brunswicker.com>
2026-05-23 13:14:44 +0200

    chore: update OSGi version to 1.0.0 in org.kttn.aem.http package-info.java

--- core/src/main/java/org/kttn/aem/http/package-info.java
+++ core/src/main/java/org/kttn/aem/http/package-info.java
@@ -5,6 +5,6 @@
  * {@link org.apache.http.impl.client.CloseableHttpClient} instances and
  * {@link org.kttn.aem.http.HttpConfigService} for the active timeout / pool / retry configuration.
  */
-@org.osgi.annotation.versioning.Version("0.9.0")
+@org.osgi.annotation.versioning.Version("1.0.0")
 @org.osgi.annotation.bundle.Export
 package org.kttn.aem.http;
